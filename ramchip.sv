f95d286670f09a9095f23b595cd7c3c8862315fe
Bobby Reyes Jr <Bobby.Reyes@ibm.com>
2021-01-01 13:56:31 -0600

    finished design

--- ramchip.sv
+++ ramchip.sv
@@ -1,17 +1,21 @@
 module ramchip #(
     parameter ADDRESS_SIZE = 64,
-    parameter WORD_SIZE = 32
+    parameter WORD_SIZE    = 32
     )(
-    input  logic address,  // ram address
-    input  logic data_in,  // input data
-    input  logic CS,       // chip select
-    input  logic WE,       // write enable
-    output logic data_out  // output data
+    input  logic [ADDRESS_SIZE-1:0] address, // ram address
+    input  logic [WORD_SIZE-1:0] data_in,    // input data
+    input  logic CS,                         // chip select
+    input  logic WE,                         // write enable
+    input  logic OE,                         // operation error
+    output logic [WORD_SIZE-1:0] data_out    // output data
     );
-
-    reg [WORD_SIZE-1:0] mem [0:ADDRESS_SIZE-1]; // create memory wordsize wide x address size deep
+      
+    //-------------//  |
+    //  WORD_SIZE  //  | ADDRESS_SIZE
+    //-------------//  V
+    logic [WORD_SIZE-1:0] mem [ADDRESS_SIZE-1:0]; // create memory wordsize wide x address size deep
     
-    // output data is continually assigned in this interation
+    // output data is continually assigned 
     assign data_out = (!CS && !OE) ? mem[address] : {WORD_SIZE{1'bz}}; 
     
 endmodule
